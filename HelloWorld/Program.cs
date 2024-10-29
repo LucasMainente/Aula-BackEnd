04cb0d55b9af56df84f47356ab1b6beb50450ffb
Lucas Mainente <mainentelucas45@gmail.com>
2024-10-29 11:06:26 -0300

    desafiosArrays

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,11 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine(@"
-                   +----------------------------------------------+
-                   |                 Menu de opcoes               |
-                   +----------------------------------------------+
-                   |- 01 - Cadastrar Cliente                      |
-                   |- 02 - Cadastrar produto                      |
-                   |- 03 - Imprimir relatorio de clientes         |
-                   |- 50 - Exibir o menu de opcoes                |
-                   |- 99 - Sair do programa                       |
-                   |----------------------------------------------|");
+﻿        
+        int[] NumeroPar = new int[6];
+        int[] NumeroImpar = new int[6];
+        int[] numeros = new int[6];
+
+        int Pares = 0, Impares = 0;
+        
+
+        for (int i = 0; i < numeros.Length; i++)
+        {
+            Console.WriteLine($"Escreva o {i + 1} º número: ");
+            int numeroDigitado = int.Parse(Console.ReadLine()!);
+            numeros[i] = numeroDigitado;
+
+            if (numeroDigitado % 2 == 0)
+            {   
+             Pares++;
+            }
+            else
+            {   
+            Impares++; 
+            }
+        }
+
+        Console.WriteLine($"\nQuantidade de números pares: {Pares}");
+        for (int i = 0; i < numeros.Length; i++)
+        {
+            if (numeros[i] % 2 == 0);
+            {
+        Console.WriteLine($"Números pares: {numeros[i]}"); 
+
+            }
+        }
+
+
+
+        Console.WriteLine($"Quantidade de números ímpares: {Impares}");
+    for (int i = 0; i < numeros.Length; i++)
+    {
+        if (numeros[i] % 2 ==1)
+        {
+        Console.WriteLine($"Números impares: {numeros[i]}"); 
+
+        }
+    }
+ 
+ 
